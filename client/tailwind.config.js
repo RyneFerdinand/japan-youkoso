99defcec0140608652d96f32112c668471b74455
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2023-05-21 18:45:28 +0900

    CODE-1: Initial client setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      light: '#F9F9F9',
+      highlight: '#BC002D',
+      dark: '#070C0D'
+    },
+    fontFamily: {
+      main: ['poppins', 'sans-serif']
+    },
+    fontSize: {
+      'head-1': [
+        '3rem',
+        {
+          fontWeight: 'bold'
+        }
+      ],
+      'head-2': [
+        '2.25rem',
+        {
+          fontWeight: 'bold'
+        }
+      ],
+      'head-3': [
+        '1.5rem',
+        {
+          fontWeight: 'bold'
+        }
+      ],
+      'head-4': [
+        '1.25rem',
+        {
+          fontWeight: 'bold'
+        }
+      ]
+    }
+  },
+  plugins: []
+}
