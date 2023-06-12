1178a91724456225f4cff9ab4b4213ef1fed3936
Antonio Battaglia <antoniobattaglia01@gmail.com>
2023-06-12 07:33:45 +0200

    Registration Page
    Home and Register button asre still to be linked. We need a button on the main page that brings to the registration

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import StepView from '../views/StepView.vue'
 import StepListView from '../views/StepListView.vue'
 import StepCreationView from '../views/StepCreationView.vue'
+import RegistrationView from '../views/RegistrationView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,12 @@ const router = createRouter({
       path: '/step/create',
       name: 'step-create',
       component: StepCreationView
+    },
+    
+    {
+      path: '/register',
+      name:'register',
+      component: RegistrationView
     }
     // {
     //   path: '/about',
